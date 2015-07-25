275029e35449b368b5c7737c43578f3436901999
justinhouse <justin@treehouse.nl>
2015-07-25 11:44:46 +0200

    Updated automation script

--- AutomationScripts/screencaptures.js
+++ AutomationScripts/screencaptures.js
@@ -14,13 +14,10 @@ target.delay(1);
 target.frontMostApp().navigationBar().leftButton().tap();
 target.delay(1);
 
-target.captureScreenWithName("ArticleDetailScreen_02")
-target.delay(1);
-
-target.frontMostApp().navigationBar().leftButton().tap();
-target.delay(1);
-
 target.frontMostApp().mainWindow().tableViews()[0].tapWithOptions({tapOffset:{x:0.47, y:0.52}});
+target.delay(1);
+
+target.captureScreenWithName("ArticleDetailScreen_02")
 target.delay(1);
 
 target.frontMostApp().navigationBar().leftButton().tap();
